dd36b16b1f6178e7dca8f0122e0a3f0e2f8f32b1
rfd <rfd@rfds-MacBook-Air.local>
2021-05-11 23:21:38 +0100

    fixed typo

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public-fluter")));
+app.use(express.static(path.join(__dirname, "public-flutter")));
 
 module.exports = app;
